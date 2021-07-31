349f77faab6c27f543891c30ed0e1dfe0f610d20
Fred Chang <fred8196@gmail.com>
2021-07-31 15:58:50 +0800

    add faq page

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -3,3 +3,10 @@ console.log('Hello!');
 $(document).ready(() => {
   console.log('HexSchool Hello!');
 });
+
+const store = document.querySelectorAll('.storeDetails')
+store.forEach(function (item) {
+  item.addEventListener("click",function (e) {
+    window.location="./store.html"
+  })
+})
