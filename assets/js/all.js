159b2a7c0fbcb22657ce4c2974ddd1e45e0f8d53
Fred Chang <fred8196@gmail.com>
2021-07-31 11:50:12 +0800

    Update 2021-07-31T03:50:09.657Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -4,4 +4,10 @@ console.log('Hello!');
 $(document).ready(function () {
   console.log('HexSchool Hello!');
 });
+var store = document.querySelectorAll('.storeDetails');
+store.forEach(function (item) {
+  item.addEventListener("click", function (e) {
+    window.location = "./store.html";
+  });
+});
 //# sourceMappingURL=all.js.map
